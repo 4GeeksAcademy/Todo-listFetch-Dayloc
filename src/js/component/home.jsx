ecc20cb650a25f8883a2eb26ec3cdeef8f847c60
Dayloc <158208881+Dayloc@users.noreply.github.com>
2024-04-27 21:55:32 +0000

    falta mostrar el map

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
   const [task,setTask] = useState('');
   const [tasks, setTasks] = useState([]);
+  const [user,setUser] = useState([])
 
-  
+  const createUser = async () => {
+    await fetch('https://playground.4geeks.com/todo/users/Carliño',{
+      method: 'POST'
+    }).then(resp => {
+      if(resp.ok){
+        alert('Se ha creado el usuario exitosamente')
+        getUser();
+      }    
+    })
+  };
+  const getUser = async ()=>{
+    await fetch('https://playground.4geeks.com/todo/users/Carliño').then(resp=>{
+      if(!resp.ok){
+        createUser()
+      }
+      return resp.json()
+    }).then(user=>setUser(user))
+  }
+  const deleteUser= async () =>{
+    await fetch('https://playground.4geeks.com/todo/users/Carliño',{
+      method: 'DELETE'
+   }).then(resp =>resp.ok && setUser(null))
+  }
+
+  useEffect(()=>{
+    getUser();
+    createUser()
+  },[])
+
+
+ 
  const validateTask = (task) => {
  if (!task || !task.trim()) {
      alert("El Campo de tarea es obligatorio.");
@@ -19,6 +50,7 @@ const Home = () => {
     const newTasks = tasks.filter((item) => item !== task);
     setTasks([...newTasks]);
     };
+   
   return (
     <div className="container text-center">
       <h1>Lista de Tareas</h1>
@@ -34,12 +66,19 @@ const Home = () => {
       <div className="con3 mt-3">
       <ul >
           {
-            tasks.map((item,index) => <li className="list"  key={index}>{index+1} .    {item} <button class="btn-close text-danger bg-danger"  onClick={()=> delateTask(item)}></button></li>)
+           tasks.map((item,index) => (
+           <li className="list"  key={item}> {index+1}
+               {item} 
+               <button class="btn-close text-danger bg-danger"  onClick={()=> delateTask(item)}>
+                </button></li>))
           }     
       </ul>
+     
       </div>
       <p className="footer mt-3">{tasks.length===0 ? 'No tienes  tareas pendientes' : <span className="pendientes">Tienes  {tasks.length} tareas pendientes. </span>}</p> 
-     
+     <button className="mb-3" onClick={()=>deleteUser()}>
+      DeleteUser
+     </button>
     </div>
   );
 };
